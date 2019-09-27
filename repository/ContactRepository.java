4af6dc45617952e3855208a4e771b381a02d261b
unknown <pummarinB6003296@gmail.com>
2019-09-27 13:43:16 +0700

    clear

--- repository/ContactRepository.java
+++
@@ -1,8 +0,0 @@
-package com.okta.spring.repository;
-
-import com.okta.spring.entity.Contact;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ContactRepository extends JpaRepository<Contact , Long>{
-
-}
