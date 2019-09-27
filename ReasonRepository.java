81778555f7a1f23b5dc77c56537790bf9fa3e809
apyoon <apple10144@hotmail.com>
2019-09-27 14:38:53 +0700

    delete

--- ReasonRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ap.app.repository;
-
-import com.ap.app.entity.Reason;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReasonRepository extends JpaRepository<Reason, Long>{
-    
-}
--- TicketBookingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ap.app.repository;
-
-import com.ap.app.entity.TicketBooking;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TicketBookingRepository extends JpaRepository<TicketBooking, Long>{
-    
-}
